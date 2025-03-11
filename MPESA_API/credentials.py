2bf5cd4a48ff8eadef4c381ea969ef96609e517c
Alvin390 <alvinkariuki07@gmail.com>
2025-03-11 13:37:30 +0300

    MPESA_API

--- MPESA_API/credentials.py
+++ MPESA_API/credentials.py
@@ -9,8 +9,18 @@ class MpesaC2bCredential:
     consumer_secret=""
     api_URL=""
 
-class MpesaAccessToke:
+class MpesaAccessToken:
     r= requests.get(MpesaC2bCredential.api_URL, auth=HTTPBasicAuth(MpesaC2bCredential.consumer_key,
                                                                    MpesaC2bCredential.consumer_secret))
     mpesa_access_token = json.loads(r.text)
     validated_mpesa_access_token = mpesa_access_token['access_token']
+
+class LipanaMpessaPpassword:
+    lipa_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    Business_short_code = ""
+    OffSetValue = ""
+    passkey = ""
+    data_to_encode = Business_short_code + passkey + lipa_time
+    online_password = base64.b64encode(data_to_encode.encode())
+    decode_password = online_password.decode('utf-8')
+    print(decode_password)
