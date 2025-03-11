6b8edde2fcb9273e7bfcdfed8dfdf60e6fa0e989
Alvin390 <alvinkariuki07@gmail.com>
2025-03-11 12:26:31 +0300

    MPESA_API

---
+++ MPESA_API/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from MPESA_API import views
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('token', views.token, name='token'),
+    path('pay', views.pay, name='pay'),
+    path('stk', views.stk, name="stk")
+
+]
